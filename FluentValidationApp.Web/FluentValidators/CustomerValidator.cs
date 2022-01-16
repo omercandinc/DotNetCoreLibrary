df001b97a2504da62d88dd36d8b952ba9bf08ce3
omercandinc <omercandinc4152@hotmail.com>
2022-01-16 16:02:28 +0300

    Add project files.

---
+++ FluentValidationApp.Web/FluentValidators/CustomerValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationApp.Web.FluentValidators
+{
+
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public String NotEmptyMessage { get; } = "{PropertyName} alanı boş olamaz";
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage(NotEmptyMessage);
+            RuleFor(x => x.Email).NotEmpty().WithMessage(NotEmptyMessage).EmailAddress().WithMessage("Email alanı doğru formatta olmalıdır.");
+
+            RuleFor(x => x.Age).NotEmpty().WithMessage(NotEmptyMessage);
+            RuleFor(x => x.BirthDay).NotEmpty().WithMessage(NotEmptyMessage).Must(x =>
+            {
+                return DateTime.Now.AddYears(-18) >= x;
+            }).WithMessage("Yaşınız 18 yaşından büyük olmalıdır.");
+        }
+    }
+}
---
+++ FluentValidationApp.Web/Migrations/20220109182117_Initial-2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FluentValidationApp.Web.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Customers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDay",
+                table: "Customers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDay",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Age",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
